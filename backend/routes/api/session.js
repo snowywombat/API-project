b7353d7d64ec296f932479ba38fab2260a488a89
Shana Edouard <shanaedouard06@gmail.com>
2022-10-25 08:57:15 -0700

    changed order of elements in routes/api/session file

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,13 +1,11 @@
 // backend/routes/api/session.js
 const express = require('express')
+const router = express.Router();
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
-
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
-
-const router = express.Router();
 
 const validateLogin = [
   check('credential')
