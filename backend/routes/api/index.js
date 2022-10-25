8a81d4cd2d3ceb68c932fd7c2a7545b5b7830df0
Shana Edouard <shanaedouard06@gmail.com>
2022-10-25 08:53:33 -0700

    cleaned up routes/api/index.js file

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,29 +1,33 @@
-// backend/routes/api/index.js
 const router = require('express').Router();
+
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const { requireAuth } = require('../../utils/auth.js');
 
 
-// // GET /api/restore-user
-const { restoreUser } = require('../../utils/auth.js');
-// Connect restoreUser middleware to the API router
-  // If current user session is valid, set req.user to the user in the database
-  // If current user session is not valid, set req.user to null
-router.use(restoreUser);
+router.get('/test', requireAuth, (req, res) => {
+  res.json({ message: 'success'})
+})
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
-router.post('/test', (req, res) => {
+// router.use('/spots', spotsRouter)
+
+router.post('/test', function(req, res) { //PHASE 1/2
   res.json({ requestBody: req.body });
 });
 
+// router.post('/test', (req, res) => {
+//   res.json({ requestBody: req.body });
+// });
 
-router.post('/test', function(req, res) { //PHASE 1/2
-    res.json({ requestBody: req.body });
-  });
-
+// // GET /api/restore-user
+// Connect restoreUser middleware to the API router
+  // If current user session is valid, set req.user to the user in the database
+  // If current user session is not valid, set req.user to null
+// router.use(restoreUser);
 
 // router.get( //PHASE 3
 //   '/restore-user',
