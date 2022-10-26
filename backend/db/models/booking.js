9e05df02d22232d27408f1da28a30ef52a37c27d
Shana Edouard <shanaedouard06@gmail.com>
2022-10-26 10:49:28 -0700

    booking model with id property

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -11,11 +11,16 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      // Booking.belongsTo(models.Spot, { foreignKey: 'spotId' })
-      // Booking.belongsTo(models.User, { foreignKey: 'userId' })
+      Booking.belongsTo(models.Spot, { foreignKey: 'spotId' })
+      Booking.belongsTo(models.User, { foreignKey: 'userId' })
     }
   }
   Booking.init({
+    id: {
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataTypes.INTEGER
+    },
     spotId: {
       type: DataTypes.INTEGER,
       allowNull: false
