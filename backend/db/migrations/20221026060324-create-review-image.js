15e0bbcfac8eb68f555ff7a45830f4c33ea500c6
Shana Edouard <shanaedouard06@gmail.com>
2022-12-23 10:42:20 -0800

    updated review images migration for render

--- backend/db/migrations/20221026060324-create-review-image.js
+++ backend/db/migrations/20221026060324-create-review-image.js
@@ -1,4 +1,12 @@
 'use strict';
+
+let options = {};
+if(process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA; //define your schema in options object
+}
+
+options.tableName = 'ReviewImages';
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -27,9 +35,9 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('ReviewImages');
+    await queryInterface.dropTable(options);
   }
 };
