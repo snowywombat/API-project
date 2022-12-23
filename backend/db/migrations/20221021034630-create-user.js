a7b3addad153413ffc0dff0a825b552461034734
Shana Edouard <shanaedouard06@gmail.com>
2022-12-23 10:37:00 -0800

    updated user migration for render

--- backend/db/migrations/20221021034630-create-user.js
+++ backend/db/migrations/20221021034630-create-user.js
@@ -1,4 +1,12 @@
 'use strict';
+
+let options = {};
+if(process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA; //define your schema in options object
+}
+
+options.tableName = 'Users';
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -33,9 +41,9 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable(options);
   }
 };
