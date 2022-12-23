8a0f15c6cbc6342565343da7beb0bddd56fd2f05
Shana Edouard <shanaedouard06@gmail.com>
2022-12-23 10:33:29 -0800

    updates to review images seeder for render

--- backend/db/seeders/20221026095336-review-image.js
+++ backend/db/seeders/20221026095336-review-image.js
@@ -1,26 +1,34 @@
 'use strict';
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if(process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA; //define your schema in options object
+}
+
+options.tableName = 'ReviewImages';
 
 const { ReviewImage, Review } = require('../models');
 
 const reviewImages = [
   {
-    rev: 'The house was so cozy and had ever we needed. Loved the deck, fire pit and bbq!!',
+    reviewId: 1,
     url: 'https://a0.muscache.com/im/pictures/f4e48fa4-df3c-4827-8ff6-6b343c3aa671.jpg?im_w=1200'
   },
   {
-    rev: 'Great spot. Spacious and comfortable.',
+    reviewId: 2,
     url: 'https://a0.muscache.com/im/pictures/miso/Hosting-9131225/original/995ec67f-a4dd-4010-ac40-926441bca80e.jpeg?im_w=1200'
   },
   {
-    rev: 'Awesome location. Host was super nice and helpful.',
+    reviewId: 3,
     url: 'https://a0.muscache.com/im/pictures/miso/Hosting-39441496/original/843cfa7a-d939-4c4c-9c85-bcff58908ae4.jpeg?im_w=1200'
   },
   {
-    rev: 'Very cute place, great location, we very much enjoyed our stay.',
+    reviewId: 4,
     url: 'https://a0.muscache.com/im/pictures/2970df4a-83c5-4a45-8b74-f7f828a35141.jpg?im_w=1200'
   },
   {
-    rev: 'Stay was great. Nice and quiet area. Very green and relaxing. Will stay again.',
+    reviewId: 5,
     url: 'https://a0.muscache.com/im/pictures/e95f1929-46fe-43f5-b20e-590ec2be9db4.jpg?im_w=1200'
   }
 
@@ -29,21 +37,11 @@ const reviewImages = [
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    for (let reviewImageInfo of reviewImages) {
-      const { url } = reviewImageInfo;
-      const foundReview = await Review.findOne({
-        where: { review: reviewImageInfo.rev }
-      });
-      await ReviewImage.create({
-        url,
-        reviewId: foundReview.id
-      });
-    }
-
+    await ReviewImage.bulkCreate(options, { validate: true });
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('ReviewImages', {}, {});
+    await queryInterface.bulkDelete(options, {}, {});
 
   }
 };
