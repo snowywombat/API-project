67757230d538b28998b622bb07762d13270c9f22
Shana Edouard <shanaedouard06@gmail.com>
2023-01-13 13:40:02 -0800

    authme phase 3 test

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -23,6 +23,20 @@ export const restoreUser = () => async dispatch => {
   return response;
 };
 
+export const login = (user) => async (dispatch) => {
+  const { credential, password } = user;
+  const response = await csrfFetch(`/api/session`, {
+    method: 'POST',
+    body: JSON.stringify({
+      credential,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 export const signup = (user) => async (dispatch) => {
   const { username, firstName, lastName, email, password } = user;
   const response = await csrfFetch("/api/users", {
@@ -40,19 +54,6 @@ export const signup = (user) => async (dispatch) => {
   return response;
 };
 
-export const login = (user) => async (dispatch) => {
-  const { credential, password } = user;
-  const response = await csrfFetch(`/api/session`, {
-    method: 'POST',
-    body: JSON.stringify({
-      credential,
-      password,
-    }),
-  });
-  const data = await response.json();
-  dispatch(setUser(data.user));
-  return response;
-};
 
 export const logout = () => async (dispatch) => {
   const response = await csrfFetch('/api/session', {
