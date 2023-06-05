893ad7e704c49acc7d426c1182bc13051384939c
Shana Edouard <shanaedouard06@gmail.com>
2023-06-04 18:54:45 -0700

    additional seeder data

--- backend/db/seeders/20221026083150-booking.js
+++ backend/db/seeders/20221026083150-booking.js
@@ -14,33 +14,273 @@ module.exports = {
       [
         {
           spotId: 1,
-          userId: 1,
-          startDate: '2022-11-03',
-          endDate: '2022-11-05'
+          userId: 2,
+          startDate: '2023-07-03',
+          endDate: '2023-07-05'
         },
         {
           spotId: 2,
-          userId: 2,
-          startDate: '2022-11-04',
-          endDate: '2022-11-08'
+          userId: 3,
+          startDate: '2023-08-04',
+          endDate: '2023-08-08'
         },
         {
           spotId: 3,
-          userId: 3,
-          startDate: '2022-10-27',
-          endDate: '2022-10-30'
+          userId: 4,
+          startDate: '2023-09-27',
+          endDate: '2023-09-30'
         },
         {
           spotId: 4,
-          userId: 4,
-          startDate: '2022-12-23',
-          endDate: '2022-12-26'
+          userId: 5,
+          startDate: '2023-07-23',
+          endDate: '2023-07-26'
         },
         {
           spotId: 5,
+          userId: 6,
+          startDate: '2023-08-04',
+          endDate: '2023-08-08'
+        },
+        {
+          spotId: 6,
+          userId: 7,
+          startDate: '2023-09-08',
+          endDate: '2023-09-12'
+        },
+        {
+          spotId: 7,
+          userId: 8,
+          startDate: '2023-07-23',
+          endDate: '2023-07-25'
+        },
+        {
+          spotId: 8,
+          userId: 9,
+          startDate: '2023-08-03',
+          endDate: '2023-08-08'
+        },
+        {
+          spotId: 9,
+          userId: 10,
+          startDate: '2023-09-13',
+          endDate: '2023-09-27'
+        },
+        {
+          spotId: 10,
+          userId: 11,
+          startDate: '2023-07-02',
+          endDate: '2023-07-05'
+        },
+        {
+          spotId: 11,
+          userId: 12,
+          startDate: '2023-08-19',
+          endDate: '2023-08-22'
+        },
+        {
+          spotId: 12,
+          userId: 13,
+          startDate: '2023-09-15',
+          endDate: '2023-09-18'
+        },
+        {
+          spotId: 13,
+          userId: 14,
+          startDate: '2023-07-21',
+          endDate: '2023-07-25'
+        },
+        {
+          spotId: 14,
+          userId: 15,
+          startDate: '2023-08-23',
+          endDate: '2023-08-27'
+        },
+        {
+          spotId: 15,
+          userId: 16,
+          startDate: '2023-09-01',
+          endDate: '2023-09-10'
+        },
+        {
+          spotId: 16,
+          userId: 17,
+          startDate: '2023-07-03',
+          endDate: '2023-07-08'
+        },
+        {
+          spotId: 17,
+          userId: 18,
+          startDate: '2023-08-14',
+          endDate: '2023-08-17'
+        },
+        {
+          spotId: 18,
+          userId: 19,
+          startDate: '2023-09-07',
+          endDate: '2023-09-12'
+        },
+        {
+          spotId: 19,
+          userId: 20,
+          startDate: '2023-07-05',
+          endDate: '2023-07-07'
+        },
+        {
+          spotId: 20,
+          userId: 19,
+          startDate: '2023-08-12',
+          endDate: '2023-08-20'
+        },
+        {
+          spotId: 21,
+          userId: 18,
+          startDate: '2023-09-03',
+          endDate: '2023-09-04'
+        },
+        {
+          spotId: 22,
+          userId: 17,
+          startDate: '2023-07-08',
+          endDate: '2023-07-09'
+        },
+        {
+          spotId: 23,
+          userId: 16,
+          startDate: '2023-08-22',
+          endDate: '2023-08-24'
+        },
+        {
+          spotId: 22,
+          userId: 17,
+          startDate: '2023-10-26',
+          endDate: '2023-10-29'
+        },
+        {
+          spotId: 21,
+          userId: 16,
+          startDate: '2023-11-06',
+          endDate: '2023-11-07'
+        },
+        {
+          spotId: 20,
+          userId: 15,
+          startDate: '2023-12-23',
+          endDate: '2023-12-29'
+        },
+        {
+          spotId: 19,
+          userId: 16,
+          startDate: '2023-10-29',
+          endDate: '2023-10-31'
+        },
+        {
+          spotId: 18,
+          userId: 15,
+          startDate: '2023-11-11',
+          endDate: '2023-11-14'
+        },
+        {
+          spotId: 17,
+          userId: 17,
+          startDate: '2023-12-02',
+          endDate: '2023-12-07'
+        },
+        {
+          spotId: 16,
+          userId: 16,
+          startDate: '2023-10-03',
+          endDate: '2023-10-05'
+        },
+        {
+          spotId: 15,
+          userId: 15,
+          startDate: '2023-11-16',
+          endDate: '2023-11-17'
+        },
+        {
+          spotId: 14,
+          userId: 13,
+          startDate: '2023-12-07',
+          endDate: '2023-12-13'
+        },
+        {
+          spotId: 13,
+          userId: 12,
+          startDate: '2023-10-14',
+          endDate: '2023-10-17'
+        },
+        {
+          spotId: 12,
+          userId: 11,
+          startDate: '2023-11-05',
+          endDate: '2023-11-10'
+        },
+        {
+          spotId: 11,
+          userId: 10,
+          startDate: '2023-12-01',
+          endDate: '2023-12-06'
+        },
+        {
+          spotId: 10,
+          userId: 9,
+          startDate: '2023-10-09',
+          endDate: '2023-10-12'
+        },
+        {
+          spotId: 9,
+          userId: 8,
+          startDate: '2023-11-04',
+          endDate: '2023-11-11'
+        },
+        {
+          spotId: 8,
+          userId: 7,
+          startDate: '2023-12-19',
+          endDate: '2023-12-22'
+        },
+        {
+          spotId: 7,
+          userId: 6,
+          startDate: '2023-10-11',
+          endDate: '2023-10-12'
+        },
+        {
+          spotId: 6,
           userId: 5,
-          startDate: '2022-12-08',
-          endDate: '2022-12-09'
+          startDate: '2023-11-06',
+          endDate: '2023-11-17'
+        },
+        {
+          spotId: 5,
+          userId: 4,
+          startDate: '2023-12-22',
+          endDate: '2023-12-26'
+        },
+        {
+          spotId: 4,
+          userId: 3,
+          startDate: '2023-10-28',
+          endDate: '2023-10-30'
+        },
+        {
+          spotId: 3,
+          userId: 2,
+          startDate: '2023-11-01',
+          endDate: '2023-11-04'
+        },
+        {
+          spotId: 2,
+          userId: 1,
+          startDate: '2023-12-05',
+          endDate: '2023-12-14'
+        },
+        {
+          spotId: 1,
+          userId: 6,
+          startDate: '2023-12-05',
+          endDate: '2023-12-14'
         },
       ],
 
--- backend/db/seeders/20221026083639-review.js
+++ backend/db/seeders/20221026083639-review.js
@@ -14,32 +14,416 @@ module.exports = {
       [
         {
           spotId: 1,
-          userId: 1,
+          userId: 2,
           review: 'The house was so cozy and had ever we needed. Loved the deck, fire pit and bbq!!',
           stars: 5
         },
         {
+          spotId: 1,
+          userId: 16,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 1,
+          userId: 9,
+          review: '',
+          stars: 5
+        },
+        {
           spotId: 2,
-          userId: 2,
-          review: 'Great spot. Spacious and comfortable.',
+          userId: 3,
+          review: 'Fair location, and the host provided standard assistance without any standout features.',
+          stars: 4
+        },
+        {
+          spotId: 2,
+          userId: 6,
+          review: '',
+          stars: 4
+        },
+        {
+          spotId: 2,
+          userId: 17,
+          review: '',
+          stars: 4
+        },
+        {
+          spotId: 3,
+          userId: 4,
+          review: 'Awesome location, host was super nice and helpful.',
           stars: 5
         },
         {
           spotId: 3,
+          userId: 10,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 3,
+          userId: 15,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 4,
+          userId: 5,
+          review: 'Very cute place, great location, we very much enjoyed our stay.',
+          stars: 5
+        },
+        {
+          spotId: 4,
+          userId: 2,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 4,
+          userId: 7,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 5,
+          userId: 6,
+          review: "The host's lack of professionalism made our stay a nightmare.",
+          stars: 1
+        },
+        {
+          spotId: 5,
+          userId: 11,
+          review: "",
+          stars: 1
+        },
+        {
+          spotId: 5,
+          userId: 20,
+          review: "",
+          stars: 1
+        },
+        {
+          spotId: 6,
+          userId: 5,
+          review: 'Decent location, and the host was generally helpful, although there were some minor issues.',
+          stars: 4
+        },
+        {
+          spotId: 6,
+          userId: 14,
+          review: '',
+          stars: 4
+        },
+        {
+          spotId: 6,
+          userId: 4,
+          review: '',
+          stars: 4
+        },
+        {
+          spotId: 7,
+          userId: 5,
+          review: 'Superb location with an exceptionally helpful and friendly host.',
+          stars: 5
+        },
+        {
+          spotId: 7,
+          userId: 13,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 7,
+          userId: 18,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 8,
+          userId: 2,
+          review: 'Perfectly situated, and the host was incredibly helpful throughout our stay.',
+          stars: 5
+        },
+        {
+          spotId: 8,
+          userId: 5,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 8,
+          userId: 19,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 9,
+          userId: 6,
+          review: "The host's lack of respect and negligence tarnished what could have been a memorable stay in a beautiful location.",
+          stars: 2
+        },
+        {
+          spotId: 9,
+          userId: 1,
+          review: "",
+          stars: 2
+        },
+        {
+          spotId: 9,
+          userId: 12,
+          review: "",
+          stars: 2
+        },
+        {
+          spotId: 10,
+          userId: 1,
+          review: "Average location, and the host's assistance was satisfactory but not exceptional.",
+          stars: 3
+        },
+        {
+          spotId: 10,
+          userId: 3,
+          review: "",
+          stars: 3
+        },
+        {
+          spotId: 10,
+          userId: 13,
+          review: "",
+          stars: 3
+        },
+        {
+          spotId: 11,
+          userId: 3,
+          review: 'With a fantastic host and a location that offered both convenience and serenity, our stay surpassed all expectations.',
+          stars: 5
+        },
+        {
+          spotId: 11,
+          userId: 16,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 11,
+          userId: 18,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 12,
+          userId: 4,
+          review: "The host's attention to detail and personal touches perfectly matched the charm of the location, creating an exceptional stay.",
+          stars: 5
+        },
+        {
+          spotId: 12,
+          userId: 5,
+          review: "",
+          stars: 5
+        },
+        {
+          spotId: 12,
+          userId: 11,
+          review: "",
+          stars: 5
+        },
+        {
+          spotId: 13,
+          userId: 5,
+          review: "Considering the fair location, the host's basic support met our needs adequately without going above and beyond.",
+          stars: 4
+        },
+        {
+          spotId: 13,
+          userId: 2,
+          review: "",
+          stars: 4
+        },
+        {
+          spotId: 13,
+          userId: 15,
+          review: "",
+          stars: 4
+        },
+        {
+          spotId: 14,
+          userId: 19,
+          review: "Despite the great location, the host's unprofessionalism and dismissive attitude left a sour taste in our mouths.",
+          stars: 2
+        },
+        {
+          spotId: 14,
+          userId: 16,
+          review: "",
+          stars: 2
+        },
+        {
+          spotId: 14,
+          userId: 17,
+          review: "",
+          stars: 2
+        },
+        {
+          spotId: 15,
+          userId: 2,
+          review: 'Stay was great. Nice and quiet area. Very green and relaxing. Will stay again.',
+          stars: 5
+        },
+        {
+          spotId: 15,
+          userId: 6,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 15,
+          userId: 10,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 16,
+          userId: 6,
+          review: 'The stunning location of the accommodation added an extra layer of magic to our stay, creating an unforgettable experience.',
+          stars: 5
+        },
+        {
+          spotId: 16,
+          userId: 7,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 16,
+          userId: 14,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 17,
+          userId: 9,
+          review: 'The picturesque setting and tranquil atmosphere of the location provided the perfect backdrop for a peaceful and rejuvenating stay.',
+          stars: 5
+        },
+        {
+          spotId: 17,
+          userId: 18,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 17,
+          userId: 20,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 18,
+          userId: 4,
+          review: 'The location was decent enough for our needs, providing a reasonable balance between accessibility and affordability.',
+          stars: 3
+        },
+        {
+          spotId: 18,
+          userId: 9,
+          review: '',
+          stars: 3
+        },
+        {
+          spotId: 18,
+          userId: 12,
+          review: '',
+          stars: 3
+        },
+        {
+          spotId: 19,
+          userId: 3,
+          review: 'Great spot. Spacious and comfortable.',
+          stars: 5
+        },
+        {
+          spotId: 19,
+          userId: 5,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 19,
+          userId: 8,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 20,
+          userId: 6,
+          review: "We appreciated the mansion's overall comfort and its serene surroundings, although a more thorough cleaning and attention to detail would have enhanced our experience.",
+          stars: 5
+        },
+        {
+          spotId: 20,
+          userId: 1,
+          review: "",
+          stars: 5
+        },
+        {
+          spotId: 20,
+          userId: 7,
+          review: "",
+          stars: 5
+        },
+        {
+          spotId: 21,
+          userId: 17,
+          review: "The mansion's serene ambiance, coupled with its seamless blend of modern comforts and classic charm, made it an idyllic retreat for our vacation.",
+          stars: 5
+        },
+        {
+          spotId: 21,
+          userId: 6,
+          review: "",
+          stars: 5
+        },
+        {
+          spotId: 21,
+          userId: 14,
+          review: "",
+          stars: 5
+        },
+        {
+          spotId: 22,
+          userId: 4,
+          review: 'Our stay at the luxurious mansion was an absolute dream come true, with its opulent architecture, lavish interiors, and breathtaking views.',
+          stars: 5
+        },
+        {
+          spotId: 22,
+          userId: 19,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 22,
+          userId: 16,
+          review: '',
+          stars: 5
+        },
+        {
+          spotId: 23,
           userId: 3,
           review: 'Awesome location. Host was super nice and helpful.',
           stars: 5
         },
         {
-          spotId: 4,
-          userId: 4,
-          review: 'Very cute place, great location, we very much enjoyed our stay.',
+          spotId: 23,
+          userId: 7,
+          review: '',
           stars: 5
         },
         {
-          spotId: 5,
-          userId: 5,
-          review: 'Stay was great. Nice and quiet area. Very green and relaxing. Will stay again.',
+          spotId: 23,
+          userId: 13,
+          review: '',
           stars: 5
         },
 
